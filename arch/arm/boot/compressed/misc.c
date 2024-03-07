07ae5abb9e08d1c454df12be4fcb9041f10841a3
haochunliang <1181558801@qq.com>
2024-03-07 10:02:20 +0800

    取消修改字节

--- arch/arm/boot/compressed/misc.c
+++ arch/arm/boot/compressed/misc.c
@@ -214,6 +214,9 @@ decompress_kernel(unsigned long output_start, unsigned long free_mem_ptr_p,
 	putstr("Uncompressing Linux...");
 	ret = do_decompress(input_data, input_data_end - input_data,
 			    output_data, error);
+
+	//
+
 	if (ret)
 		error("decompressor returned an error");
 	else
